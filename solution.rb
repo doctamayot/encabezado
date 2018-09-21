5f957aa18e2e5ed19f2d833650ad57c2ef1a7c95
Hugo Tamayo <doctamayot@hotmail.com>
2018-09-20 21:41:24 -0500

    sube

--- solution.rb
+++ solution.rb
@@ -1,7 +1,5 @@
 require 'sinatra'
-require 'useragent'
+
 get '/' do
-    string = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.56 Safari/536.5'
-    user_agent = UserAgent.parse(string)
-  "#{user_agent.browser}"
+    "#{request.user_agent}"
 end
